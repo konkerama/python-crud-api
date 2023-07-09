4a0787e4a34df34c95a08d8247e188839127739d
konkeramaris <konkeramaris@gmail.com>
2023-07-09 19:59:59 +0300

    upd

--- README.md
+++ README.md
@@ -6,3 +6,72 @@ pipenv run pip freeze > requirements.txt
 ```
 
 skaffold dev --trigger=manual
+
+
+python local testing 
+``` bash 
+docker compose up --build 
+```
+
+python status:
+- done basic connection to mongodb and added some items
+
+todo:
+- retrieve items from mongo db and send them back via api
+- read a bit in general about how mongo db works
+  - https://www.guru99.com/what-is-mongodb.html
+  - https://www.mongodb.com/languages/python
+  - https://www.mongodb.com/basics/database-index
+- do the same for postgresdb
+
+
+
+## API spec
+### Postgres
+```
+POST /pg/customer
+{
+    customer_name="sdf"
+}
+curl -X POST localhost:8080/pg/customer -d '{"customer_name":"mark"}' -H "Content-Type: application/json"
+```
+```
+GET /pg/customer?customer_name="sdf"
+curl localhost:8080/pg/customer?customer_name=mark
+```
+
+### Mongo
+```
+GET /mongo/orders?product_name="asdf"
+curl localhost:8080/mongo/orders?product_name=banana
+```
+```
+POST /mongo/orders
+{
+    customer_id="sdf",
+    product_name="asd"
+}
+curl -X POST localhost:8080/mongo/orders -d '{"customer_id":"2", "product_name":"apple"}' -H "Content-Type: application/json"
+```
+
+## todo:
+handle gp db connections properly using SQLAlchemy
+https://stackoverflow.com/questions/55523299/best-practices-for-persistent-database-connections-in-python-when-using-flask
+https://python-adv-web-apps.readthedocs.io/en/latest/flask_db1.html
+https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/quickstart/#installation
+https://docs.sqlalchemy.org/en/20/intro.html
+
+
+
+urls:
+- https://www.dataquest.io/blog/tutorial-connect-install-and-query-postgresql-in-python/
+- https://pynative.com/python-postgresql-insert-update-delete-table-data-to-perform-crud-operations/#h-python-postgresql-insert-into-database-table
+- https://www.geeksforgeeks.org/postgresql-serial/
+- https://stackoverflow.com/questions/21524482/psycopg2-typeerror-not-all-arguments-converted-during-string-formatting
+
+
+update dict:
+- https://www.freecodecamp.org/news/add-to-dict-in-python/
+
+mongo jsonify (we use the bjon library that is built in the psycopg2-binary):
+- https://stackoverflow.com/questions/16586180/typeerror-objectid-is-not-json-serializable
--- python/app/main.py
+++ python/app/main.py
@@ -9,6 +9,7 @@ from sys import stdout
 import requests
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
+import json
 
 
 from opentelemetry import trace
@@ -17,22 +18,19 @@ from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-OTEL_URL = os.environ['OTEL_EXPORTER_OTLP_ENDPOINT']
-
+from pymongo import MongoClient
+from pandas import DataFrame
+import psycopg2
+from bson import json_util
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(name)-12s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
-
 logFormatter = logging.Formatter("[%(asctime)s] %(name)-12s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
-
-
 logger = logging.getLogger('werkzeug')
 logger.setLevel(logging.INFO)
 consoleHandler = logging.StreamHandler(stdout) #set streamhandler to stdout
 consoleHandler.setFormatter(logFormatter)
-logger.addHandler(consoleHandler)
+# logger.addHandler(consoleHandler)
 config = helper.read_config()
-
-
 
 resource = Resource(attributes={
 SERVICE_NAME: "client",
@@ -40,51 +38,134 @@ SERVICE_NAME: "client",
 "env.name": "prod"
 })
 
-
-
 provider = TracerProvider(resource=resource)
 processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://opentelemetry-collector.monitoring.svc.cluster.local:4317"))
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 
-
-
-# tracer_provider = trace.get_tracer_provider()
-# tracer = tracer_provider.get_tracer(__name__)
-# # tracer.set_attribute(SERVICE_NAME, "your-service-name")
-# processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://simplest-collector.monitoring.svc.cluster.local:4318"))
-# tracer_provider.add_span_processor(processor)
-# # FlaskInstrumentor().instrument_app(flask)
-
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
-print ("asdf")
 app.wsgi_app = OpenTelemetryMiddleware(app.wsgi_app, tracer_provider=provider)
 
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 RequestsInstrumentor().instrument()
 
 
-# @tracer.start_as_current_span("get_server")
-def get_foo():
-    x = requests.get('http://foo1:5678')
-    return x
+MONGODB_USERNAME= os.environ['ME_CONFIG_MONGODB_ADMINUSERNAME']
+MONGODB_PASSWD= os.environ['ME_CONFIG_MONGODB_ADMINPASSWORD']
+POSTGRES_USER= os.environ['POSTGRES_USER']
+POSTGRES_PASSWORD= os.environ['POSTGRES_PASSWORD']
+POSTGRES_DB= os.environ['POSTGRES_DB']
+POSTGRES_URL= os.environ['POSTGRES_URL']
+
+conn = psycopg2.connect(database=POSTGRES_DB,
+                        host=POSTGRES_URL,
+                        user=POSTGRES_USER,
+                        password=POSTGRES_PASSWORD,
+                        port="5432")
+
+def pg_connection():
+    if conn.closed():
+        return conn
+    conn = psycopg2.connect(database=POSTGRES_DB,
+                            host=POSTGRES_URL,
+                            user=POSTGRES_USER,
+                            password=POSTGRES_PASSWORD,
+                            port="5432")
+    return conn
+
+conn = pg_connection()
+
+def get_database():
+    CONNECTION_STRING = f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWD}@mongodb/"
+    client = MongoClient(CONNECTION_STRING)
+    return client['orders']
+
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
 
 @app.route('/', methods = ['GET', 'POST'])
-# @tracer.start_as_current_span("client-home")
 def home():
     if(request.method == 'GET'):
         logger.info('sample log')
-        # carrier = {}
-        # TraceContextTextMapPropagator().inject(carrier)
-        # header = {"traceparent": carrier["traceparent"]}
-        # logger.info(header)
-        x = get_foo()
-        logger.info(x.status_code)
-        logger.info(x.text)
-        data = f"Hello scaffold from {x.text}"
-        return jsonify({'data': data,'lambda-response': x.text})
+        data = f"Hello scaffold from asd"
+        return jsonify({'data': data,'lambda-response': 'asd'})
     return jsonify({'request': 'POST'})
+
+@app.route('/pg/table', methods = ['POST'])
+def pg_table():
+    if(request.method == 'POST'):
+        logger.info('pg post table')
+        cursor = conn.cursor()
+        command = """ CREATE TABLE customers (
+                customer_id SERIAL PRIMARY KEY,
+                customer_name VARCHAR(255) NOT NULL
+                )
+        """
+        cursor.execute(command)
+        cursor.close()
+        conn.commit()
+        return jsonify({'status': 'created'})
+    return jsonify({'request': 'POST'})
+
+@app.route('/pg/customer', methods = ['GET', 'POST'])
+def pg_customer():
+    if(request.method == 'GET'):
+        logger.info('pg customer get')
+        logger.info(print(request.query_string))
+        try: 
+            customer_name = request.args.get('customer_name')
+            logger.info (f"querying postgres for customer: {customer_name}")
+            cursor = conn.cursor()
+            command = f"SELECT * FROM customers WHERE customer_name='{customer_name}'"
+            cursor.execute(command)
+            # cursor.close()
+            conn.commit()
+        catch: 
+
+        result = cursor.fetchall()
+        logger.info(result)
+        return result
+    else:
+        logger.info('pg customer post')
+        logger.info(request.form)
+        logger.info(request.json)
+        customer_name = request.json.get('customer_name')
+        logger.info (f"creating customer: {customer_name}")
+        cursor = conn.cursor()
+        postgres_insert_query = "INSERT INTO customers (customer_name) VALUES (%s)"
+        record_to_insert = (customer_name,)
+        cursor.execute(postgres_insert_query, record_to_insert)
+        conn.commit()
+        logger.info(f"customer {customer_name} inserted")
+        return jsonify({'customer': customer_name,'status': 'inserted'})
+
+@app.route('/mongo/orders', methods = ['GET', 'POST'])
+def mongo_orders():
+    if(request.method == 'GET'):
+        dbname = get_database()
+        collection_name = dbname["orders"]
+        product_name = request.args.get('product_name')
+        item_details = collection_name.find({"product_name" : product_name})
+        items_df = DataFrame(item_details).transpose()
+        output = parse_json(items_df.to_dict())
+        logger.info(output)
+        return output
+    else:
+        dbname = get_database()
+        collection_name = dbname["orders"]
+        customer_id = request.json.get('customer_id')
+        product_name = request.json.get('product_name')
+        item = {
+        "customer_id" : customer_id,
+        "product_name" : product_name,
+        }
+        collection_name.insert_one(item)
+        logger.info(f"item {item} inserted")
+        output=parse_json(dict(item, status="inserted"))
+        logger.info(output)
+        return output
 
 @app.route('/home/<int:num>', methods = ['GET'])
 def disp(num):
