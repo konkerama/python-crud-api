9893795e0abcf15d6f4272059845beef2448dba9
konkeramaris <konkeramaris@gmail.com>
2023-07-16 17:51:56 +0300

    feat: renamed client

--- python/test.sh
+++ python/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL=$(minikube service client -n orders --url)
+URL=$(minikube service production-client -n orders --url)
 curl -X POST $URL/pg/customer -d '{"customer_name":"mark"}' -H "Content-Type: application/json"
 curl $URL/pg/customer?customer_name=mark
 curl -X POST $URL/mongo/orders -d '{"customer_id":"2", "product_name":"apple"}' -H "Content-Type: application/json"
